a52c5521ba59881fb675b469e10a1ce278bff303
Tomas Rawski <rtomas@gmail.com>
2024-07-18 12:20:29 -0300

    fix func issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import UniversalProvider from "@walletconnect/universal-provider";
 import { WalletConnectModal } from "@walletconnect/modal";
 import { useEffect, useState } from "react";
-//import { signMessage,signTransacion, getBalance, TronChains } from "./utils/helpers";
 
 import { TronService, TronChains } from "./utils/tronService";
 
@@ -21,50 +20,74 @@ const modal = new WalletConnectModal({
   chains,
 });
 
-// 4. create provider instance
-const provider = await UniversalProvider.init({
-  logger: "error",
-  projectId: projectId,
-  metadata: {
-    name: "WalletConnect x Tron",
-    description: "Tron integration with WalletConnect's Universal Provider",
-    url: "https://walletconnect.com/",
-    icons: ["https://avatars.githubusercontent.com/u/37784886"],
-  },
-});
 
-const tronService = new TronService(provider);
 
 const App = () => {
   const [isConnected, setIsConnected] = useState(false);
+  const [address, setAddress] = useState("");
   const [balance, setBalance] = useState<number | null>(null);
+  const [provider, setProvider] = useState<UniversalProvider | null>(null);
+  const [tronService, setTronService] = useState<TronService | null>(null);
+
+// --- 
+
+
+
+  useEffect(() => {
+    async function setOnInitProvider() {
+      const providerValue = await UniversalProvider.init({
+        logger: "error",
+        projectId: projectId,
+        metadata: {
+          name: "WalletConnect x Tron",
+          description: "Tron integration with WalletConnect's Universal Provider",
+          url: "https://walletconnect.com/",
+          icons: ["https://avatars.githubusercontent.com/u/37784886"],
+        },
+      });
+        
+      setProvider(providerValue);
+    }
+
+    setOnInitProvider();
+  }, []);
+
+  useEffect(() => {
+    if (!provider) return;
+    const tronServiceValue = new TronService(provider);
+    
+    provider.on("display_uri", async (uri: string) => {
+      console.log("uri", uri);
+      await modal.openModal({
+        uri,
+      });
+    });
+
+    // get address once loaded
+    setAddress(provider.session?.namespaces.tron?.accounts[0].split(":")[2]!);
+    setTronService(tronServiceValue);
+  }, [provider]);
+
 
   useEffect(() => {
     async function  getBalanceInit() {
+      if (!tronService) return;
       const res = await tronService.getBalance(address!);
+
       setBalance(res!);
     }
     
     if (!isConnected) return; 
     getBalanceInit()
-  }, [isConnected]);
-
-  // 5. get address once loaded
-  const address =
-    provider.session?.namespaces.tron?.accounts[0].split(":")[2];
+  }, [isConnected, tronService]);
 
   // 6. handle display_uri event and open modal
-  provider.on("display_uri", async (uri: string) => {
-    console.log("uri", uri);
-    await modal.openModal({
-      uri,
-    });
-  });
+  
 
   // 7. handle connect event
   const connect = async () => {
     try {
-      await provider.connect({
+      await provider!.connect({
         namespaces: {
           tron: {
             methods,
@@ -74,7 +97,7 @@ const App = () => {
         },
       });
       setIsConnected(true);
-      console.log("session", provider.session);
+      console.log("session", provider!.session);
     } catch {
       console.log("Something went wrong, request cancelled");
     }
@@ -83,14 +106,14 @@ const App = () => {
 
   // 8. handle disconnect event
   const disconnect = async () => {
-    await provider.disconnect();
+    await provider!.disconnect();
     setIsConnected(false);
   };
 
   // 9. handle signMessage and sendTransaction
   const handleSign = async () => {
     console.log("signing");
-    const res = await tronService.signMessage(
+    const res = await tronService!.signMessage(
       `Can i have authorize this request pls - ${Date.now()}`,
       address!
     );
@@ -99,7 +122,7 @@ const App = () => {
 
     // 10. handle get Balance
     const handleGetBalance = async () => {
-      const res = await tronService.getBalance(address!);
+      const res = await tronService!.getBalance(address!);
       console.log(res);
       setBalance(res);
     };
@@ -107,7 +130,7 @@ const App = () => {
 
   const handleSendTransaction = async () => {
     console.log("signing");
-    const res = await tronService.signTransaction(address!, 100);
+    const res = await tronService!.signTransaction(address!, 100);
     console.log("result send tx: ", res);
   };
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
-  build: {
-    target: ['esnext', 'chrome89', 'edge89', 'firefox89', 'safari15']
-  }
+  plugins: [react(), nodePolyfills()]
 });
